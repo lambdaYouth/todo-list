fe02fea74ba01fb140531aa06e86bc60c4057db0
lambdaYouth <krishnanhsujith1705@gmail.com>
2025-06-13 11:43:54 +0530

    Done some work

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,8 +2,6 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
